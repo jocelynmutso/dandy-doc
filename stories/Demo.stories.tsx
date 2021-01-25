1496e0a53c6966fb1e65dca7646e6b5bdd72718c
Jocelyn Mutso (ext) <jocelyn.mutso@resys.io>
2021-01-25 15:53:18 +0200

    init

---
+++ stories/Demo.stories.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+import { Paper } from '@material-ui/core';
+
+
+export default { title: 'Demo stories' };
+
+export const hello = () => (<>
+  <Paper> hello demo </Paper>
+</>);
