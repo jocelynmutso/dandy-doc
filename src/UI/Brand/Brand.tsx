b737be12fbfa5ec18aea4ffd6de41fb710e72cd4
Jocelyn Mutso (ext) <jocelyn.mutso@resys.io>
2021-01-26 15:07:14 +0200

    cleanup

---
+++ src/UI/Brand/Brand.tsx
@@ -0,0 +1,37 @@
+import React from 'react';
+
+import Typography from '@material-ui/core/Typography';
+import { fade, Theme, makeStyles } from '@material-ui/core/styles';
+
+
+
+const useStyles = makeStyles((theme: Theme) => ({
+  iconLink:{
+    '&:hover': {
+      cursor: 'pointer',
+    },
+    color: theme.palette.text.primary,
+    alignContent: 'center',
+    paddingLeft: theme.spacing(5)
+  }
+}));
+
+
+interface BrandProps {
+  image?: HTMLImageElement,
+  title: string
+}
+
+const Brand: React.FC<BrandProps> = ({image, title}) => {
+  const classes = useStyles();
+  
+  
+  return (<React.Fragment>
+      {image}
+      <Typography variant="body1" noWrap className={classes.iconLink} style={{ flex: 1 }} >
+        {title}
+      </Typography>
+  </React.Fragment>);
+}
+
+export default Brand;
